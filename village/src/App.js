72701791317a51975100a24dbb94bbc25f3720d2
nanoblit <nanoblit@users.noreply.github.com>
2019-06-14 09:55:07 +0200

    Add post

--- village/src/App.js
+++ village/src/App.js
@@ -7,11 +7,23 @@ import Smurfs from './components/Smurfs';
 
 const App = () => {
   const [smurfs, setSmurfs] = useState([]);
+  const [inputData, setInputData] = useState({ name: '', age: '', height: '' });
+  const smurfsUrl = 'http://localhost:3333/smurfs';
 
   const getSmurfs = async () => {
     try {
-      const res = await axios.get('http://localhost:3333/smurfs');
+      const res = await axios.get(smurfsUrl);
       setSmurfs(res.data);
+      console.log(smurfs);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const postSmurf = async () => {
+    try {
+      await axios.post(smurfsUrl, inputData);
+      getSmurfs();
       console.log(smurfs);
     } catch (err) {
       console.log(err.message);
@@ -22,13 +34,31 @@ const App = () => {
     getSmurfs();
   }, []);
 
+  const addSmurf = e => {
+    e.preventDefault();
+    postSmurf();
+    setInputData({ name: '', age: '', height: '' });
+  };
+
+  const handleInputChange = e => {
+    const newData = { ...inputData };
+    newData[e.target.name] = e.target.value;
+    setInputData(newData);
+  };
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data
   // coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   return (
     <div className="App">
-      <SmurfForm />
+      <SmurfForm
+        addSmurf={addSmurf}
+        handleInputChange={handleInputChange}
+        name={inputData.name}
+        age={inputData.age}
+        height={inputData.height}
+      />
       <Smurfs smurfs={smurfs} />
     </div>
   );
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,57 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SmurfForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      age: '',
-      height: '',
-    };
-  }
-
-  addSmurf = event => {
-    event.preventDefault();
-    // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: '',
-    });
-  };
-
-  handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  render() {
-    return (
-      <div className="SmurfForm">
-        <form onSubmit={this.addSmurf}>
-          <input
-            onChange={this.handleInputChange}
-            placeholder="name"
-            value={this.state.name}
-            name="name"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="age"
-            value={this.state.age}
-            name="age"
-          />
-          <input
-            onChange={this.handleInputChange}
-            placeholder="height"
-            value={this.state.height}
-            name="height"
-          />
-          <button type="submit">Add to the village</button>
-        </form>
-      </div>
-    );
-  }
-}
+const SmurfForm = ({
+  addSmurf, handleInputChange, name, age, height,
+}) => (
+  <div className="SmurfForm">
+    <form onSubmit={addSmurf}>
+      <input onChange={handleInputChange} placeholder="name" value={name} name="name" />
+      <input onChange={handleInputChange} placeholder="age" value={age} name="age" />
+      <input onChange={handleInputChange} placeholder="height" value={height} name="height" />
+      <button type="submit">Add to the village</button>
+    </form>
+  </div>
+);
 
 export default SmurfForm;
